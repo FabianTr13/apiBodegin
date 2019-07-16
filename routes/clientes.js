046ee3dc4243bd90cfdbed31121d19ee666e4d7f
fabian <afabian.escoto@gmail.com>
2019-07-15 23:18:27 -0600

    midleware catcha

--- routes/clientes.js
+++ routes/clientes.js
@@ -5,7 +5,7 @@ var mdAunt = require('../midleware/auth-middleware')
 
 /* uso de middleware */
 router.use(mdAunt.content_type)
-router.use(mdAunt.captchaCheck)
+// router.use(mdAunt.captchaCheck)
 // router.use(mdAunt.token_validation)
 
 /* POST page. */
