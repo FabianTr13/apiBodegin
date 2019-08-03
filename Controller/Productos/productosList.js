d88c8eebc514cc7c48253bf35bcda976bdb93ba9
fabian <afabian.escoto@gmail.com>
2019-08-03 00:21:14 -0600

    clientes servicios

--- Controller/Productos/productosList.js
+++ Controller/Productos/productosList.js
@@ -4,7 +4,8 @@ exports.productosList = async function(req, res, next) {
   let producto =
     await pg.func('app.ft_proc_devuelve_productos_list_servicio',
       [
-        req.body.id_servicio
+        req.body.id_servicio,
+        req.body.token
       ]).catch(err => {
         console.log(err);
       res.status(500).send({
