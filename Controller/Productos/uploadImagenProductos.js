b8322924865150b939840ca28beeefe2e03b64d0
Alex Escoto <afabian.escoto@gmail.com>
2019-08-10 10:15:17 -0600

    api

--- Controller/Productos/uploadImagenProductos.js
+++ Controller/Productos/uploadImagenProductos.js
@@ -5,11 +5,14 @@ exports.uploadImagenProductos = async function(req, res, next) {
     return res.status(400).send('No files were uploaded.');
   }
 
+  var val = await Math.floor(1000 + Math.random() * 9000);
+
   let image = req.files.image;
-  let filename = 'producto_'+req.body.id_producto+'.jpg'
+  let filename =  `producto_${req.body.id_producto}${val}.jpg`
+  console.log(filename);
 
   let url_imagen = await pg.func('app.ft_proc_devuelve_url_save')
-  // console.log(url_imagen);
+
   image.mv(`${url_imagen[0].ft_proc_devuelve_url_save}${filename}`, function(err) {
     if (err)
       return res.status(500).send(err);
