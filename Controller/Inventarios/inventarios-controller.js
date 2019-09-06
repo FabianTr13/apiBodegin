b72d906b4de45874b4c2de4f8695f303a5479040
Alex Escoto <afabian.escoto@gmail.com>
2019-09-06 13:56:32 -0600

    reprotes

--- Controller/Inventarios/inventarios-controller.js
+++ Controller/Inventarios/inventarios-controller.js
@@ -1,6 +1,10 @@
 var insertTransaccion = require('./insertTransaccion');
 var getChart = require('./getChart');
+var reporteStock = require('./reporteStock');
+var reportePedido = require('./reportePedido');
 
 //Exports encabezado de funciones
 exports.insertTransaccion = insertTransaccion.insertTransaccion;
 exports.getChart = getChart.getChart;
+exports.reporteStock = reporteStock.reporteStock;
+exports.reportePedido = reportePedido.reportePedido;
---
+++ Controller/Inventarios/reportePedido.js
@@ -0,0 +1,21 @@
+const pg = require('../../config/ps_connection')
+
+exports.reportePedido = async function(req, res, next) {
+  let inventario =
+    await pg.func('app.ft_proc_devuelve_reporte_punto_pedido',
+      [
+        req.body.token
+      ]).catch(err => {
+        console.log(err);
+      res.status(500).send({
+        error: err,
+        status: 500
+      });
+    })
+
+  if (res.statusCode != 200) {
+    return
+  }
+  // console.log(inventario);
+  res.send(inventario[0].ft_proc_devuelve_reporte_punto_pedido)
+}
